90514b7aac1a51d3972772892839e78cc42da038
michelecandido <mmarilia.sc@gmail.com>
2026-05-21 17:37:08 -0300

    feat: adicionado sistema completo

--- jogodaforca_projetofinal.py
+++ jogodaforca_projetofinal.py
@@ -14,16 +14,47 @@ underscores = []
 for i in palavra:
     i = "_"
     underscores.append(i)
-print("Palavra: "+ " ".join(underscores))
 
 vidas = 6
 ja_tentadas = []
 letras_erradas = []
 
 while(True):
+    print()
     print("Palavra: "+ " ".join(underscores))
-    print(f"Letras erradas: "+ ", ".join(underscores))
+    if(letras_erradas):
+        print(f"Letras erradas: "+ ", ".join(letras_erradas))
+    else:
+        print(f"Letras erradas: nenhuma")
     print(f"Vidas restantes: {vidas}")
 
+    # chute
     chute_letra = input("Digite uma letra: ").lower()
-    break
+
+    if(chute_letra not in ja_tentadas):
+        ja_tentadas.append(chute_letra)
+    else:
+        print("Você já usou essa letra! Tente outra.")
+        continue
+
+    for indice,i in enumerate(palavra):
+        if(i == chute_letra):
+            underscores[indice] = chute_letra
+    
+    # letra certa ou nao
+    if(chute_letra in palavra):
+        print("Letra correta!")
+
+        if("_" not in underscores):
+            print("\nPalavra: "+ " ".join(underscores))
+            print(f"Parabéns! Você venceu! A palavra era: {palavra}")
+            break
+    else:
+        if(chute_letra not in letras_erradas):
+            letras_erradas.append(chute_letra)
+            vidas = vidas - 1
+        print(f"Letra errada! Você perdeu uma vida: {vidas}")
+
+        if(vidas == 0):
+            print(f"Você perdeu! A palavra certa era: {palavra}")
+            break
